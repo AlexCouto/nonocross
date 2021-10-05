501149c6f9e9c3eba135d1b52b5fed0e5585ab15
AlexCouto <64182631+AlexCouto@users.noreply.github.com>
2021-10-05 15:27:22 -0300

    titulo 'Nonocross' no header (provisório)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,17 @@ import { makeStyles } from '@material-ui/styles';
 const useStyles = makeStyles(theme => ({
     root: {
         height: 50,
-        backgroundColor: '#2C3E50'
+        backgroundColor: '#1c1c20'
     },
     left_header: {
         float: 'left'
+    },
+    title: {
+        float:'left',
+        color: 'white',
+        fontFamily:'Helvetica',
+        fontSize: 30,
+        marginTop: 8
     }
 }));
 
@@ -19,6 +26,9 @@ export default function Header(){
         <section className={classes.root}>
             <section className={classes.left_header}>
                 <SideMenu />
+            </section>
+            <section className={classes.title}>
+                NonoCross
             </section>
         </section>
     )
