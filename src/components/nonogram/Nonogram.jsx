d8f860ad1154ab51b298d00108d9a3664731529a
AlexCouto <64182631+AlexCouto@users.noreply.github.com>
2021-12-09 15:42:17 -0300

    cor com teclado, coordenada hover

--- src/components/nonogram/Nonogram.jsx
+++ src/components/nonogram/Nonogram.jsx
@@ -6,6 +6,7 @@ import Clues from './Clues'
 import Grid from './Grid'
 import { useResultMatrixContext } from "../../context/ResultMatrixContext"
 import HoverContextProvider from "../../context/HoverContext"
+import colorLuminance  from "../../colorLuminance";
 
 export default function Nonogram(props) {
 
@@ -14,6 +15,7 @@ export default function Nonogram(props) {
   const [colorMatrix, setColorMatrix] = useState(
     resultMatrix.map((row) => (row.map(() => ("empty"))))
   )
+  const [fontColor , setfontColor] = useState({})
 
   const [penColor, setPenColor] = useState(
     "empty"
@@ -41,9 +43,28 @@ export default function Nonogram(props) {
     setColorMatrix( resultMatrix.map((row) => (row.map(() => ("empty")))))
     setPenColor("empty")
     setColorArrayStatus(colorArray.map(()=>false))
+    setPenColor(colorArray[0])
   } , [resultMatrix, colorArray] )
   
+  // Definindo cor da fonte do texto dentro da clue e do color selector
+  useEffect( () => {
+    let fontcolorDict = {}
+    for(let i = 0 ; i < colorArray.length ; i++)
+    {
+      let text_color
+      let cellColor = colorArray[i]
 
+      // a partir da luminância da cor, escolher entre preto ou branco a cor de fonte que possuir
+      // maior de contraste conforme https://www.w3.org/WAI/GL/wiki/Contrast_ratio
+      if( colorLuminance(cellColor) > 0.179)
+        text_color = "#000000"
+      else
+        text_color = "#ffffff"
+      
+      fontcolorDict[cellColor] = text_color
+    }
+    setfontColor(fontcolorDict)
+  } , [resultMatrix , colorArray])
   
 
   // const [selectedColor, setSelectedColor] = useState(
@@ -77,6 +98,19 @@ export default function Nonogram(props) {
     })
   }
 
+  function onKeyboardPress(event){
+    let key = parseInt(event.key,10)
+    if(!isNaN(key) && key <= colorArray.length && key > 0 )
+    {
+      setColorArrayStatus((colorArrayStatus) => {
+        colorArrayStatus = colorArrayStatus.fill(false)
+        colorArrayStatus[key-1] = true
+        return [...colorArrayStatus]
+      })
+      setPenColor(colorArray[key-1])
+    }
+  }
+
   function onCSClick(e, color, i) {
     setColorArrayStatus((colorArrayStatus) => {
       colorArrayStatus = colorArrayStatus.fill(false)
@@ -86,19 +120,19 @@ export default function Nonogram(props) {
     setPenColor(color)
   }
 
-  return <div>
-    <ColorSelector colorArray={colorArray} colorArrayStatus={colorArrayStatus} 
+  return <div onKeyPress={onKeyboardPress}>
+    <ColorSelector colorArray={colorArray} colorArrayStatus={colorArrayStatus} fontColor={fontColor}
       onColorSelectClick={onCSClick}/>
     <div className="nono_container">
       <table className="nono_table nono_top">
         <HoverContextProvider>
           <tbody>
             <tr className="nono_tr">
-              <td className="nono_td nono_top">{/*<ColorToggler/>*/}</td>
-              <td className="nono_td nono_top"><Clues type="top" size={nonoSize} resultMatrix={resultMatrix} colorArray={colorArray}/></td>
+              <td className="nono_td nono_top" style={{backgroundColor: penColor}}>{/*<ColorToggler/>*/}</td>
+              <td className="nono_td nono_top"><Clues type="top" size={nonoSize} resultMatrix={resultMatrix} colorArray={colorArray} fontColor={fontColor}/></td>
             </tr>
             <tr className="nono_tr">
-              <td className="nono_td nono_top"><Clues type="left" size={nonoSize} resultMatrix={resultMatrix} colorArray={colorArray} /></td>
+              <td className="nono_td nono_top"><Clues type="left" size={nonoSize} resultMatrix={resultMatrix} colorArray={colorArray} fontColor={fontColor}/></td>
               <td className="nono_td nono_top"><Grid onCellClick={onCellClick} colorMatrix={colorMatrix}/></td>
             </tr>
           </tbody>
